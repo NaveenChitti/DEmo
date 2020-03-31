25a386bf27064be1f61c81225e15af1d5985b0a8
nchitti <nchitti@paypal.com>
2020-03-31 11:12:56 +0530

    new file Multiplication_Table.java

---
+++ src/main/java/Multiplication_Table.java
@@ -0,0 +1,13 @@
+public class Multiplication_Table
+{
+    public static void main(String[] args)
+    {
+
+        int n = 9;
+
+        for(int i=1; i <= 10; i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
