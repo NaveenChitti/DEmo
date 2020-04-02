d2c6a783e9a08c42c13271bc1146b0a539ba8435
nchitti <nchitti@paypal.com>
2020-04-02 17:11:41 +0530

    changed the value of variable num from 23 to 27

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,13 +2,13 @@ public class Test {
 
     public static void main(String[] args) {
 
-        int n = 10;
+        int value = 10;
         int t1 = 0;
         int t2 = 1;
 
-        System.out.print("First " + n + " terms: ");
+        System.out.print("First " + value + " terms: ");
 
-        for (int i = 1; i <= n; ++i)
+        for (int i = 1; i <= value; ++i)
         {
             System.out.print(t1 + " ");
 
